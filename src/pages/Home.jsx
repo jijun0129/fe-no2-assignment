5b3a150f2eac35af83552dd9f018a57152bc8b9c
Injun Jeong <jijun897@naver.com>
2025-05-19 22:01:21 +0900

    Feat: Home page 제작

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,45 @@
+import styled from "styled-components";
+import PokemonLogo from "../assets/pokemon-logo.png";
+import { useNavigate } from "react-router-dom";
+
+const ContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Img = styled.img`
+  width: 600px;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  font-size: 20px;
+  background-color: #ff0000;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #cc0000;
+  }
+`;
+
 const Home = () => {
-  return;
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/dex");
+  };
+
+  return (
+    <ContainerDiv>
+      <Img src={PokemonLogo} />
+      <Button onClick={handleButtonClick}>포켓몬 도감 시작하기</Button>
+    </ContainerDiv>
+  );
 };
 
 export default Home;
