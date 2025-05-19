9482cc124d019890476f5b944237dc410aec9470
Injun Jeong <jijun897@naver.com>
2025-05-19 22:01:04 +0900

    Design: 폰트 적용 및 전역 스타일링 적용

---
+++ src/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: #ffe4b8;
+    box-sizing: border-box;
+}
+`;
+
+export default GlobalStyle;
